2980ca5d2205257447f13dcfbbc571fce25f6879
chhacker <chhacker@kali>
2023-11-25 11:51:02 -0500

    F**k this shit.

--- backend/api.py
+++ backend/api.py
@@ -1,29 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from typing import List
+from functions import *
 
 app = FastAPI()
 
 # Sample data structure to store game actions
 games = {
-    "game1": []
+    "Game1": []
     # Add more games as needed...
 }
+players = {
+    "player1": Player("You", 100),
+    "player2": Player("Bot1", 100),
+    "player3": Player("Bot2", 100)
+}
 
-# Endpoint to handle game actions
-@app.get('/api/game/{game_id}/actions', response_model=List[str])
-def get_game_actions(game_id: str):
-    if game_id not in games:
-        return {"error": "Game ID not found"}
+@app.post('/api/flag')
+def receive_flag(flag: bool):
+    print(f"received flag.")
+    return{"message": "flag received"}
 
-    return games[game_id]
+@app.post("/api/front_message/")
+def receive_message(message: str):
+    int(receive_message)
 
-@app.post('/api/game/{game_id}/actions')
+@app.post('/api/flag')
 def add_game_action(game_id: str, action: str):
     if game_id not in games:
         return {"error": "Game ID not found"}
 
-    if action not in ['fold', 'raise', 'bet']:
-        return {"error": "Invalid action"}
+    match action:
+        case "fold":
+            Player.fold()
+
+        case "bet":
+            Player.bet()
+
+        case "raise_bet":
+            Player.raise_bet()
+
+        case _:
+            Print("Invalid option")
 
     # Append the action to the respective game's actions list
     games[game_id].append(action)
--- backend/functions.py
+++ backend/functions.py
@@ -1,13 +1,13 @@
 class Player:
-    def init(self, name, chips):
+    def __init__(self, name, chips):
         self.name = name
         self.chips = chips
         self.hand = []
         self.in_round = True
 
-    def bet(self, amount):
+    def bet(self):
         if amount > self.chips:
-            print(f"{self.name} doesn't have enough chips to bet {amount}.")
+            print(f"{self.name} don't have enough chips to bet {amount}.")
             return False
         self.chips -= amount
         print(f"{self.name} bets {amount} chips.")
@@ -17,9 +17,9 @@ class Player:
         self.in_round = False
         print(f"{self.name} folds.")
 
-    def raise_bet(self, amount):
+    def raise_bet(self):
         if amount > self.chips:
-            print(f"{self.name} doesn't have enough chips to raise by {amount}.")
+            print(f"{self.name} don't have enough chips to raise by {amount}.")
             return False
         self.chips -= amount
         print(f"{self.name} raises by {amount} chips.")
