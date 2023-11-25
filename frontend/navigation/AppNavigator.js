6cf996e2eb66dfcb0304bf86ff8c2b0a237d60fd
konstantin <konstantinmuehlberg@gmail.com>
2023-11-25 15:23:39 +0100

    added other stuff

---
+++ frontend/navigation/AppNavigator.js
@@ -0,0 +1,23 @@
+// AppNavigator.js
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import GameScreen from '../components/GameScreen';
+import LobbyScreen from '../components/LobbyScreen';
+import AuthScreen from '../components/AuthScreen';
+
+const AppNavigator = createStackNavigator(
+  {
+    Lobby: LobbyScreen,
+    Game: GameScreen,
+    Auth: AuthScreen,
+  },
+    {
+        initialRouteName: 'Lobby',
+        // initialRouteName: 'Game',
+  }
+//   {
+//     initialRouteName: 'Auth',
+//   }
+);
+
+export default createAppContainer(AppNavigator);
