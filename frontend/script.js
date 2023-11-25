14f6951cfacfbf782327ca3fa8e7a12653cacee0
konstantin <konstantinmuehlberg@gmail.com>
2023-11-25 17:30:15 +0100

    new code

---
+++ frontend/script.js
@@ -0,0 +1,8 @@
+// Add functionality here using JavaScript
+document.getElementById('dealBtn').addEventListener('click', function() {
+    // Functionality to deal cards
+    // This could involve generating random cards for players and community cards
+  });
+  
+  // Add other event listeners for fold, check, raise, etc.
+  
