28bf45ce2f9557fb18d12fc4b4b7238ae010dee0
konstantin <konstantinmuehlberg@gmail.com>
2023-11-25 14:16:29 +0100

    first commit

---
+++ frontend/src/components/GameScreen.js
@@ -0,0 +1,14 @@
+// GameScreen.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const GameScreen = () => {
+  return (
+    <View>
+      <Text>Game Screen</Text>
+      {/* Add game-related components */}
+    </View>
+  );
+};
+
+export default GameScreen;
