30827710c56d3b2a1ff01799503405047782da3c
ian-fisher <yanfisher13@gmail.com>
2023-11-25 22:26:36 +0100

    Complete rework. Solo backend leeet's gooo

--- backend/api.py
+++ backend/api.py
@@ -3,46 +3,61 @@ from typing import List
 from functions import *
 
 app = FastAPI()
+players
+currBet
+startPlayer = -1
 
-# Sample data structure to store game actions
-games = {
-    "Game1": []
-    # Add more games as needed...
-}
-players = {
-    "player1": Player("You", 100),
-    "player2": Player("Bot1", 100),
-    "player3": Player("Bot2", 100)
-}
+def enemies_move(enemie):
+	if players[enemie].in_game == False:
+		pass
+	probability = randrange(0, 20)
+	match probability:
+		case p if p < 4:
+			players[enemie].fold()
+		case 20:
+			if players[enemie].chips > currBet:
+				players[enemie].bet(min(players[enemie].chips, currBet*3))
+			return;
+		case p2 if p2 > 17:
+			if players[enemie].chips > currBet:
+				players[enemie].bet(min(players[enemie].chips, currBet*2,5))
+			return;
+		case p2 if p2 > 13:
+			if players[enemie].chips > currBet:
+				players[enemie].bet(min(players[enemie].chips, currBet*2))
+			return;
+		case p2 if p2 > 8:
+			if players[enemie].chips > currBet:
+				players[enemie].bet(min(players[enemie].chips, currBet*1,5))
+			return;
+		case _:
+			if players[enemie].chips > currBet:
+				players[enemie].bet(currBet)
 
-@app.post('/api/flag')
-def receive_flag(flag: bool):
-    print(f"received flag.")
-    return{"message": "flag received"}
+@app.post("../api/gameInit")
+def game_start(message: str):
+	currBet = 10
+	startPlayer += 1
+	poker_deck = [str(rank)+str(suit) for suit in suits for rank in ranks]
+	used_cards = []
+	players = [Player(message), Player("Bot1"), Player("Bot2")]
+	
+	res = []
+	for player in players:
+		res.append(player.hand)
+	return res
 
-@app.post("/api/front_message/")
-def receive_message(message: str):
-    int(receive_message)
+@app.post("../api/game")
+def game(message: str):
+	for player in range(startPlayer, startPlayer+len(players)):
+		if(player%len(players) == 0):
+			return True
+		enemies_move(players[player%len(players)])
 
-@app.post('/api/flag')
-def add_game_action(game_id: str, action: str):
-    if game_id not in games:
-        return {"error": "Game ID not found"}
-
-    match action:
-        case "fold":
-            Player.fold()
-
-        case "bet":
-            Player.bet()
-
-        case "raise_bet":
-            Player.raise_bet()
-
-        case _:
-            Print("Invalid option")
-
-    # Append the action to the respective game's actions list
-    games[game_id].append(action)
-
-    return {"message": "Action added successfully"}
+@app.post("../api/action")
+def on_action(message: str):
+	int(receive_message)
+	if message[0] == 'b':
+		players[0].bet(int(message[5:]))
+	else:
+		players[0].fold()
--- backend/functions.py
+++ backend/functions.py
@@ -1,26 +1,31 @@
+from random import randrange
+
+suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'Ja', 'Qu', 'Ki', 'Ac']
+
+# Generate a list of all poker cards
+poker_deck = [str(rank)+str(suit) for suit in suits for rank in ranks]
+used_cards = []
+
+def get():
+	i = randrange(0, len(poker_deck))
+	used_cards.append(poker_deck[i])
+	del poker_deck[i]
+	return used_cards[-1]
+	
 class Player:
-    def __init__(self, name, chips):
+    def __init__(self, name):
         self.name = name
-        self.chips = chips
-        self.hand = []
+        self.chips = 100
+        self.hand = [get(), get()]
         self.in_round = True
 
-    def bet(self):
+    def bet(self, amount):
+        self.in_round = True
         if amount > self.chips:
-            print(f"{self.name} don't have enough chips to bet {amount}.")
             return False
         self.chips -= amount
-        print(f"{self.name} bets {amount} chips.")
         return True
 
     def fold(self):
         self.in_round = False
-        print(f"{self.name} folds.")
-
-    def raise_bet(self):
-        if amount > self.chips:
-            print(f"{self.name} don't have enough chips to raise by {amount}.")
-            return False
-        self.chips -= amount
-        print(f"{self.name} raises by {amount} chips.")
-        return True
