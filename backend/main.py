1ffaa89acefae4f41d3bef2cabbb324f0f74f0ec
ian-fisher <yanfisher13@gmail.com>
2023-11-26 14:38:41 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ import json
 import threading
 import socket
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket = socket.socket()
 server_address = ('127.0.0.1', 8089)  # Replace with desired address and port
 server_socket.bind(server_address)
 
